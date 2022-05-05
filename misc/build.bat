e6b3f969357f20d1835f228908e99f958b1d0ac6
rafik rizik <rizik944@gmail.com>
2022-05-05 11:39:00 -0500

    ok

--- misc/build.bat
+++ misc/build.bat
@@ -1,8 +1,10 @@
 @echo off
 
 set clang_cl_flags=-Z7 -Wno-writable-strings
+set win32_link=user32.lib gdi32.lib
 
 IF NOT EXIST ..\build mkdir ..\build
 pushd ..\build
-clang-cl %clang_cl_flags% ..\code\main.cpp 
+rem clang-cl %clang_cl_flags% ..\code\main.cpp 
+clang-cl %clang_cl_flags% ..\code\win_platform.cpp -link %win32_link%
 popd
